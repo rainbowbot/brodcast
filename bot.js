b0818a9d70a3d0e1cfee61b5aea929e3fc62007b
botmakerdiscord <43237951+botmakerdiscord@users.noreply.github.com>
2018-09-14 14:40:07 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,25 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+var prefix=`!`
+ 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+ 
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+ 
+ 
 client.on('message', message => {
     if (message.author.id === client.user.id) return;
     if (message.guild) {
    let embed = new Discord.RichEmbed()
     let args = message.content.split(' ').slice(1).join(' ');
 if(message.content.split(' ')[0] == prefix - 'bc') {
-    message.channel.send(`\`${message.guild.memberCount}\` : عدد الاعضاء المستلمين`); 
+    message.channel.send(`\`${message.guild.memberCount}\` : عدد الاعضاء المستلمين`);
     if (!args[1]) {
 return;
 }
@@ -23,3 +38,5 @@ return;
         return;
     }
 });
+ 
+client.login(process.env.BOT_TOKEN)
